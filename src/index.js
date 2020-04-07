b54cb075cf7c2892b7c73c2e0edea9ce47329335
honey badger <uasergey@hotmail.com>
2020-04-07 23:31:35 +0300

    13

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import './css/styles.css';
-import "../node_modules/pnotify/dist/PNotifyBrightTheme.css";
-import './js/find_country.js'
+import './styles.css';
+import './CSS/basiclightbox.scss';
+import './../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+import './JS/main.js';
+import './JS/infScroll.js';
+import './JS/basiclightbox.js';
